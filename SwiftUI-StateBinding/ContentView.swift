27331beed7aebab3e7cca99c13a0feb47533f6db
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2021-01-06 00:03:21 +0530

    Chapter 7 Complete ✌🏻

--- SwiftUI-StateBinding/ContentView.swift
+++ SwiftUI-StateBinding/ContentView.swift
@@ -9,15 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var isPlaying: Bool = false
+    @State private var ButtonOneCount: Int = 0
+    @State private var ButtonTwoCount: Int = 0
+    @State private var ButtonThreeCount: Int = 0
+    @State private var TotalClickCount: Int = 0
+    var body: some View {
+        
+        VStack {
+            
+            Text("\(TotalClickCount)")
+                .font(.system(size: 200, design: .rounded))
+            
+            HStack {
+                ButtonWithCounter(clickCount: $ButtonOneCount, totalClicks: $TotalClickCount, bgColor: Color.red)
+                ButtonWithCounter(clickCount: $ButtonTwoCount, totalClicks: $TotalClickCount, bgColor: Color.blue)
+                ButtonWithCounter(clickCount: $ButtonThreeCount, totalClicks: $TotalClickCount, bgColor: Color.green)
+                
+            }
+        }
+    }
+}
+
+struct ButtonWithCounter: View {
+    @Binding var clickCount: Int
+    @Binding var totalClicks: Int
+    var bgColor: Color
     
     var body: some View {
         Button (action: {
-            isPlaying.toggle()
+            clickCount += 1
+            totalClicks += 1
         }, label: {
-            Image(systemName: isPlaying ? "play.circle.fill" : "stop.circle.fill")
-                .font(.system(size: 150))
-                .foregroundColor(isPlaying ? .green : .red)
+            
+            Circle()
+                .frame(width: 100, height: 100)
+                .foregroundColor(bgColor)
+                .overlay(
+                    Text("\(clickCount)")
+                        .font(.system(size: 50, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                )
         })
     }
 }
