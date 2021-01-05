4cecc992f0301c7d234148bca19c426be8f4bfc8
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2021-01-05 23:11:28 +0530

    Initial Commit

---
+++ SwiftUI-StateBinding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-StateBinding
+//
+//  Created by Pradyumn Shukla on 05/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-StateBinding/SwiftUI_StateBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StateBindingApp.swift
+//  SwiftUI-StateBinding
+//
+//  Created by Pradyumn Shukla on 05/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StateBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
